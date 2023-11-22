8d59dbbd2acc4ff10520ec137fc88d16e7e2c2e5
Vignesh Venkat <vigneshv@google.com>
2023-11-21 20:52:54 -0800

    tests yaay!

---
+++ tests/decoder_tests.rs
@@ -0,0 +1,81 @@
+use rust_libavif::*;
+
+const TEST_DATA_PATH: &str = "/Users/vigneshv/code/rust_pg/rust-libavif/tests/data";
+
+fn get_test_file(filename: &str) -> String {
+    String::from(format!("{TEST_DATA_PATH}/{filename}"))
+}
+
+fn get_decoder(filename: &str) -> decoder::AvifDecoder {
+    let abs_filename = get_test_file(filename);
+    let mut decoder = decoder::AvifDecoder::default();
+    let res = decoder
+        .set_io_file(&abs_filename)
+        .expect("Failed to set IO");
+    decoder
+}
+
+macro_rules! assert_avif_error {
+    ($res:ident, $err:ident) => {
+        assert!($res.is_err());
+        assert!(matches!($res.err().unwrap(), AvifError::$err));
+    };
+}
+
+// From avifalphanoispetest.cc
+#[test]
+fn alpha_no_ispe() {
+    // See https://github.com/AOMediaCodec/libavif/pull/745.
+    let mut decoder = get_decoder("alpha_noispe.avif");
+    // By default, non-strict files are refused.
+    assert!(matches!(decoder.settings.strictness, AvifStrictness::All));
+    let res = decoder.parse();
+    assert_avif_error!(res, BmffParseFailed);
+    // Allow this kind of file specifically.
+    decoder.settings.strictness =
+        AvifStrictness::SpecificExclude(vec![AvifStrictnessFlag::AlphaIspeRequired]);
+    let res = decoder.parse();
+    assert!(res.is_ok());
+    assert!(decoder.alpha_present);
+    let res = decoder.next_image();
+    assert!(res.is_some());
+    let image = res.unwrap();
+    let alpha_plane = image.plane(3);
+    assert!(alpha_plane.is_some());
+    assert!(alpha_plane.unwrap().row_bytes > 0);
+}
+
+// From avifanimationtest.cc
+#[test]
+fn animated_image() {
+    let mut decoder = get_decoder("colors-animated-8bpc.avif");
+    let res = decoder.parse();
+    assert!(res.is_ok());
+    assert!(!decoder.alpha_present);
+    //assert!(!decoder.image_sequence_track_present);
+    assert_eq!(decoder.image_count, 5);
+    assert_eq!(decoder.repetition_count, 0);
+    for _ in 0..5 {
+        assert!(decoder.next_image().is_some());
+    }
+}
+
+// From avifanimationtest.cc
+#[test]
+fn animated_image_with_source_set_to_primary_item() {
+    let mut decoder = get_decoder("colors-animated-8bpc.avif");
+    decoder.settings.source = decoder::AvifDecoderSource::PrimaryItem;
+    let res = decoder.parse();
+    assert!(res.is_ok());
+    assert!(!decoder.alpha_present);
+    //assert!(!decoder.image_sequence_track_present);
+    // imageCount is expected to be 1 because we are using primary item as the
+    // preferred source.
+    assert_eq!(decoder.image_count, 1);
+    assert_eq!(decoder.repetition_count, 0);
+    // Get the first (and only) image.
+    assert!(decoder.next_image().is_some());
+    // Subsequent calls should not return anything since there is only one
+    // image in the preferred source.
+    assert!(decoder.next_image().is_none());
+}
