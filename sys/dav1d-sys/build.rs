acb8b64232583cd40fc1f10842ab3d29eaacc309
Vignesh Venkatasubramanian <vigneshv@google.com>
2024-01-30 15:08:45 -0800

    dav1d-sys: remove generalizations in build script

--- sys/dav1d-sys/build.rs
+++ sys/dav1d-sys/build.rs
@@ -9,7 +9,44 @@ fn path_buf(inputs: &[&str]) -> PathBuf {
     path
 }
 
-pub fn generate_bindings(header_file: PathBuf, version_dir: PathBuf, outfile: PathBuf) {
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
+    let build_target = std::env::var("TARGET").unwrap();
+    let build_dir = if build_target.contains("android") {
+        if build_target.contains("x86_64") {
+            "build.android/x86_64"
+        } else if build_target.contains("x86") {
+            "build.android/x86"
+        } else if build_target.contains("aarch64") {
+            "build.android/aarch64"
+        } else if build_target.contains("arm") {
+            "build.android/arm"
+        } else {
+            panic!("Unknown target_arch for android. Must be one of x86, x86_64, arm, aarch64.");
+        }
+    } else {
+        "build"
+    };
+
+    let library_path = path_buf(&[build_dir, "src"]);
+    let project_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let abs_library_dir = PathBuf::from(&project_root).join("dav1d");
+    let abs_object_dir = PathBuf::from(&abs_library_dir).join(library_path);
+    let object_file = format!("libdav1d.a");
+    let library_file = PathBuf::from(&abs_object_dir).join(object_file);
+    if !Path::new(&library_file).exists() {
+        panic!("dav1d not found. Run dav1d.cmd.");
+    }
+    println!("cargo:rustc-link-search={}", abs_object_dir.display());
+    println!("cargo:rustc-link-lib=static=dav1d");
+
+    // Generate bindings.
+    let header_file =
+        PathBuf::from(&abs_library_dir).join(path_buf(&["include", "dav1d", "dav1d.h"]));
+    let version_dir =
+        PathBuf::from(&abs_library_dir).join(path_buf(&[build_dir, "include", "dav1d"]));
+    let outfile = PathBuf::from(&project_root).join(path_buf(&["src", "dav1d.rs"]));
     let extra_includes_str = format!("-I{}", version_dir.display());
     let mut bindings = bindgen::Builder::default()
         .header(header_file.into_os_string().into_string().unwrap())
@@ -40,58 +77,5 @@ pub fn generate_bindings(header_file: PathBuf, version_dir: PathBuf, outfile: Pa
     println!(
         "cargo:rustc-env=CRABBYAVIF_DAV1D_BINDINGS_RS={}",
         outfile.display()
-    );
-}
-
-fn add_native_library(
-    library_dir: &str,
-    library_path: PathBuf,
-    header_file: PathBuf,
-    extra_include_dir: PathBuf,
-    bindings_path: PathBuf,
-) {
-    let project_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let abs_library_dir = PathBuf::from(&project_root).join(library_dir);
-    let abs_object_dir = PathBuf::from(&abs_library_dir).join(library_path);
-    let object_file = format!("libdav1d.a");
-    let library_file = PathBuf::from(&abs_object_dir).join(object_file);
-    if !Path::new(&library_file).exists() {
-        panic!("dav1d not found. Run dav1d.cmd.");
-    }
-    println!("cargo:rustc-link-search={}", abs_object_dir.display());
-    println!("cargo:rustc-link-lib=static=dav1d");
-
-    let abs_header_file = PathBuf::from(&abs_library_dir).join(header_file);
-    let extra_includes = PathBuf::from(&abs_library_dir).join(extra_include_dir);
-    let outfile = PathBuf::from(&project_root).join(bindings_path);
-    generate_bindings(abs_header_file, extra_includes, outfile);
-}
-
-fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-
-    let build_target = std::env::var("TARGET").unwrap();
-    let build_dir = if build_target.contains("android") {
-        if build_target.contains("x86_64") {
-            "build.android/x86_64"
-        } else if build_target.contains("x86") {
-            "build.android/x86"
-        } else if build_target.contains("aarch64") {
-            "build.android/aarch64"
-        } else if build_target.contains("arm") {
-            "build.android/arm"
-        } else {
-            panic!("Unknown target_arch for android. Must be one of x86, x86_64, arm, aarch64.");
-        }
-    } else {
-        "build"
-    };
-
-    add_native_library(
-        "dav1d",
-        path_buf(&[build_dir, "src"]),
-        path_buf(&["include", "dav1d", "dav1d.h"]),
-        path_buf(&[build_dir, "include", "dav1d"]),
-        path_buf(&["src", "dav1d.rs"]),
     );
 }
