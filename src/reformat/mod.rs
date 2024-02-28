6e233f57a09136598457e5ff3f3b405db6d3e932
Vignesh Venkat <vigneshv@google.com>
2024-02-28 12:06:50 -0800

    Remove some unnecessary return statements

--- src/reformat/mod.rs
+++ src/reformat/mod.rs
@@ -17,38 +17,38 @@ pub mod libyuv {
     use crate::*;
 
     pub fn yuv_to_rgb(_image: &image::Image, _rgb: &mut rgb::Image) -> AvifResult<bool> {
-        return Err(AvifError::NotImplemented);
+        Err(AvifError::NotImplemented)
     }
 
     pub fn process_alpha(_rgb: &mut rgb::Image, _multiply: bool) -> AvifResult<()> {
-        return Err(AvifError::NotImplemented);
+        Err(AvifError::NotImplemented)
     }
 
     pub fn convert_to_half_float(_rgb: &mut rgb::Image, _scale: f32) -> AvifResult<()> {
-        return Err(AvifError::NotImplemented);
+        Err(AvifError::NotImplemented)
     }
 
     impl image::Image {
         pub fn alpha_to_full_range(&mut self) -> AvifResult<()> {
-            return Err(AvifError::NotImplemented);
+            Err(AvifError::NotImplemented)
         }
         pub fn scale(&mut self, _width: u32, _height: u32) -> AvifResult<()> {
-            return Err(AvifError::NotImplemented);
+            Err(AvifError::NotImplemented)
         }
     }
 
     impl rgb::Image {
         pub fn premultiply_alpha(&mut self) -> AvifResult<()> {
-            return Err(AvifError::NotImplemented);
+            Err(AvifError::NotImplemented)
         }
         pub fn unpremultiply_alpha(&mut self) -> AvifResult<()> {
-            return Err(AvifError::NotImplemented);
+            Err(AvifError::NotImplemented)
         }
         pub fn set_opaque(&mut self) -> AvifResult<()> {
-            return Err(AvifError::NotImplemented);
+            Err(AvifError::NotImplemented)
         }
         pub fn import_alpha_from(&mut self, _image: &image::Image) -> AvifResult<()> {
-            return Err(AvifError::NotImplemented);
+            Err(AvifError::NotImplemented)
         }
     }
 }
