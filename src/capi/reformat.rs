webmproject/CrabbyAvif

commit 7a4c72c30ab5c08b88f9a6d077db75cbe0385ceb

Rewrite pointer access and remove unnecessary intermediates (#50)
This is a large scale re-write that does the following:
1) Ensure there are only two ways to access pixel data (as a plane
   pointer or as a row reference).
2) Get rid of the intermediate optional slice types and use
   specific row* functions in each caller.

src/internal_utils/pixels.rs
@@ -2,21 +2,21 @@ use crate::internal_utils::*;
 use crate::*;
 
 pub enum Pixels {
-    // Intended for use from the C API. Used for all bitdepths.
+    // Intended for use from the C API. Used for 8-bit images.
     Pointer(*mut u8),
+    // Intended for use from the C API. Used for 10-bit, 12-bit and 16-bit images.
+    Pointer16(*mut u16),
     // Used for 8-bit images.
     Buffer(Vec<u8>),
     // Used for 10-bit, 12-bit and 16-bit images.
     Buffer16(Vec<u16>),
 }
 
-pub type U8OrU16Slice<'a> = (Option<&'a [u8]>, Option<&'a [u16]>);
-pub type U8OrU16SliceMut<'a> = (Option<&'a mut [u8]>, Option<&'a mut [u16]>);
-
 impl Pixels {
     pub fn size(&self) -> usize {
         match self {
             Pixels::Pointer(_) => 0,
+            Pixels::Pointer16(_) => 0,
             Pixels::Buffer(buffer) => buffer.len(),
             Pixels::Buffer16(buffer) => buffer.len(),
         }
@@ -25,6 +25,7 @@ impl Pixels {
     pub fn pixel_bit_size(&self) -> usize {
         match self {
             Pixels::Pointer(_) => 0,
+            Pixels::Pointer16(_) => 0,
             Pixels::Buffer(_) => 8,
             Pixels::Buffer16(_) => 16,
         }
@@ -33,6 +34,7 @@ impl Pixels {
     pub fn has_data(&self) -> bool {
         match self {
             Pixels::Pointer(ptr) => !ptr.is_null(),
+            Pixels::Pointer16(ptr) => !ptr.is_null(),
             Pixels::Buffer(buffer) => !buffer.is_empty(),
             Pixels::Buffer16(buffer) => !buffer.is_empty(),
         }
@@ -41,6 +43,7 @@ impl Pixels {
     pub fn resize(&mut self, size: usize, default: u16) -> AvifResult<()> {
         match self {
             Pixels::Pointer(_) => return Err(AvifError::InvalidArgument),
+            Pixels::Pointer16(_) => return Err(AvifError::InvalidArgument),
             Pixels::Buffer(buffer) => {
                 if buffer.capacity() < size && buffer.try_reserve_exact(size).is_err() {
                     return Err(AvifError::OutOfMemory);
@@ -58,13 +61,46 @@ impl Pixels {
     }
 
     pub fn is_pointer(&self) -> bool {
-        matches!(self, Pixels::Pointer(_))
+        matches!(self, Pixels::Pointer(_) | Pixels::Pointer16(_))
     }
 
-    pub fn pointer(&self) -> *mut u8 {
+    pub fn ptr(&self) -> *const u8 {
+        match self {
+            Pixels::Pointer(ptr) => *ptr as *const u8,
+            Pixels::Buffer(buffer) => buffer.as_ptr(),
+            _ => std::ptr::null_mut(),
+        }
+    }
+
+    pub fn ptr16(&self) -> *const u16 {
+        match self {
+            Pixels::Pointer16(ptr) => *ptr as *const u16,
+            Pixels::Buffer16(buffer) => buffer.as_ptr(),
+            _ => std::ptr::null_mut(),
+        }
+    }
+
+    pub fn ptr_mut(&mut self) -> *mut u8 {
         match self {
             Pixels::Pointer(ptr) => *ptr,
+            Pixels::Buffer(buffer) => buffer.as_mut_ptr(),
             _ => std::ptr::null_mut(),
+        }
+    }
+
+    pub fn ptr16_mut(&mut self) -> *mut u16 {
+        match self {
+            Pixels::Pointer16(ptr) => *ptr,
+            Pixels::Buffer16(buffer) => buffer.as_mut_ptr(),
+            _ => std::ptr::null_mut(),
+        }
+    }
+
+    pub fn clone_pointer(&self) -> Option<Pixels> {
+        match self {
+            Pixels::Pointer(ptr) => Some(Pixels::Pointer(*ptr)),
+            Pixels::Pointer16(ptr) => Some(Pixels::Pointer16(*ptr)),
+            _ => None,
         }
     }
 
@@ -76,6 +112,7 @@ impl Pixels {
                 let offset = isize_from_usize(offset)?;
                 Ok(unsafe { std::slice::from_raw_parts(ptr.offset(offset), size) })
             }
+            Pixels::Pointer16(_) => Err(AvifError::NoContent),
             Pixels::Buffer(buffer) => {
                 let end = offset.checked_add(size).ok_or(AvifError::NoContent)?;
                 Ok(&buffer[offset..end])
@@ -92,6 +129,7 @@ impl Pixels {
                 let offset = isize_from_usize(offset)?;
                 Ok(unsafe { std::slice::from_raw_parts_mut(ptr.offset(offset), size) })
             }
+            Pixels::Pointer16(_) => Err(AvifError::NoContent),
             Pixels::Buffer(buffer) => {
                 let end = offset.checked_add(size).ok_or(AvifError::NoContent)?;
                 Ok(&mut buffer[offset..end])
@@ -104,7 +142,8 @@ impl Pixels {
         let offset: usize = usize_from_u32(offset)?;
         let size: usize = usize_from_u32(size)?;
         match self {
-            Pixels::Pointer(ptr) => {
+            Pixels::Pointer(_) => Err(AvifError::NoContent),
+            Pixels::Pointer16(ptr) => {
                 let offset = isize_from_usize(offset)?;
                 let ptr = (*ptr) as *const u16;
                 Ok(unsafe { std::slice::from_raw_parts(ptr.offset(offset), size) })
@@ -121,9 +160,10 @@ impl Pixels {
         let offset: usize = usize_from_u32(offset)?;
         let size: usize = usize_from_u32(size)?;
         match self {
-            Pixels::Pointer(ptr) => {
+            Pixels::Pointer(_) => Err(AvifError::NoContent),
+            Pixels::Pointer16(ptr) => {
                 let offset = isize_from_usize(offset)?;
-                let ptr = (*ptr) as *mut u16;
+                let ptr = *ptr;
                 Ok(unsafe { std::slice::from_raw_parts_mut(ptr.offset(offset), size) })
             }
             Pixels::Buffer(_) => Err(AvifError::NoContent),
@@ -131,42 +171,6 @@ impl Pixels {
                 let end = offset.checked_add(size).ok_or(AvifError::NoContent)?;
                 Ok(&mut buffer[offset..end])
             }
-        }
-    }
-
-    pub fn slices(&self, offset: u32, size: u32) -> AvifResult<U8OrU16Slice> {
-        match self {
-            Pixels::Pointer(ptr) => {
-                let offset = isize_from_u32(offset)?;
-                let size = usize_from_u32(size)?;
-                let ptr16 = (*ptr) as *const u16;
-                unsafe {
-                    Ok((
-                        Some(std::slice::from_raw_parts(ptr.offset(offset), size)),
-                        Some(std::slice::from_raw_parts(ptr16.offset(offset), size)),
-                    ))
-                }
-            }
-            Pixels::Buffer(_) => Ok((Some(self.slice(offset, size)?), None)),
-            Pixels::Buffer16(_) => Ok((None, Some(self.slice16(offset, size / 2)?))),
-        }
-    }
-
-    pub fn slices_mut(&mut self, offset: u32, size: u32) -> AvifResult<U8OrU16SliceMut> {
-        match self {
-            Pixels::Pointer(ptr) => {
-                let offset = isize_from_u32(offset)?;
-                let size = usize_from_u32(size)?;
-                let ptr16 = (*ptr) as *mut u16;
-                unsafe {
-                    Ok((
-                        Some(std::slice::from_raw_parts_mut(ptr.offset(offset), size)),
-                        Some(std::slice::from_raw_parts_mut(ptr16.offset(offset), size)),
-                    ))
-                }
-            }
-            Pixels::Buffer(_) => Ok((Some(self.slice_mut(offset, size)?), None)),
-            Pixels::Buffer16(_) => Ok((None, Some(self.slice16_mut(offset, size / 2)?))),
         }
     }
 }

src/capi/image.rs
@@ -184,20 +184,18 @@ impl From<&Image> for avifImage {
             if !image.has_plane(i.into()) {
                 continue;
             }
-            let plane = image.plane(i.into()).unwrap();
-            dst_image.yuvPlanes[i] = if image.depth == 8 {
-                plane.data.unwrap().as_ptr() as *mut u8
+            dst_image.yuvPlanes[i] = if image.depth > 8 {
+                image.planes2[i].as_ref().unwrap().ptr16() as *mut u8
             } else {
-                plane.data16.unwrap().as_ptr() as *mut u8
+                image.planes2[i].as_ref().unwrap().ptr() as *mut u8
             };
             dst_image.yuvRowBytes[i] = image.row_bytes[i];
         }
         if image.has_plane(Plane::A) {
-            let plane = image.plane(Plane::A).unwrap();
-            dst_image.alphaPlane = if image.depth == 8 {
-                plane.data.unwrap().as_ptr() as *mut u8
+            dst_image.alphaPlane = if image.depth > 8 {
+                image.planes2[3].as_ref().unwrap().ptr16() as *mut u8
             } else {
-                plane.data16.unwrap().as_ptr() as *mut u8
+                image.planes2[3].as_ref().unwrap().ptr() as *mut u8
             };
             dst_image.alphaRowBytes = image.row_bytes[3];
         }

src/codecs/dav1d.rs
@@ -143,7 +143,11 @@ impl Decoder for Dav1d {
                 image.width = dav1d_picture.p.w as u32;
                 image.height = dav1d_picture.p.h as u32;
                 image.depth = dav1d_picture.p.bpc as u8;
-                image.planes2[3] = Some(Pixels::Pointer(dav1d_picture.data[0] as *mut u8));
+                image.planes2[3] = Some(if image.depth > 8 {
+                    Pixels::Pointer16(dav1d_picture.data[0] as *mut u16)
+                } else {
+                    Pixels::Pointer(dav1d_picture.data[0] as *mut u8)
+                });
                 image.row_bytes[3] = dav1d_picture.stride[0] as u32;
                 image.image_owns_planes[3] = false;
                 let seq_hdr = unsafe { &(*dav1d_picture.seq_hdr) };
@@ -170,8 +174,11 @@ impl Decoder for Dav1d {
                 image.matrix_coefficients = (seq_hdr.mtrx as u16).into();
 
                 for plane in 0usize..image.yuv_format.plane_count() {
-                    image.planes2[plane] =
-                        Some(Pixels::Pointer(dav1d_picture.data[plane] as *mut u8));
+                    image.planes2[plane] = Some(if image.depth > 8 {
+                        Pixels::Pointer16(dav1d_picture.data[plane] as *mut u16)
+                    } else {
+                        Pixels::Pointer(dav1d_picture.data[plane] as *mut u8)
+                    });
                     let stride_index = if plane == 0 { 0 } else { 1 };
                     image.row_bytes[plane] = dav1d_picture.stride[stride_index] as u32;
                     image.image_owns_planes[plane] = false;

src/codecs/libgav1.rs
@@ -108,7 +108,11 @@ impl Decoder for Libgav1 {
                     image.width = gav1_image.displayed_width[0] as u32;
                     image.height = gav1_image.displayed_height[0] as u32;
                     image.depth = gav1_image.bitdepth as u8;
-                    image.planes2[3] = Some(Pixels::Pointer(gav1_image.plane[0] as *mut u8));
+                    image.planes2[3] = Some(if image.depth > 8 {
+                        Pixels::Pointer16(gav1_image.plane[0] as *mut u16)
+                    } else {
+                        Pixels::Pointer(gav1_image.plane[0] as *mut u8)
+                    });
                     image.row_bytes[3] = gav1_image.stride[0] as u32;
                     image.image_owns_planes[3] = false;
                     image.full_range =
@@ -138,8 +142,11 @@ impl Decoder for Libgav1 {
                     image.matrix_coefficients = (gav1_image.matrix_coefficients as u16).into();
 
                     for plane in 0usize..image.yuv_format.plane_count() {
-                        image.planes2[plane] =
-                            Some(Pixels::Pointer(gav1_image.plane[plane] as *mut u8));
+                        image.planes2[plane] = Some(if image.depth > 8 {
+                            Pixels::Pointer16(gav1_image.plane[plane] as *mut u16)
+                        } else {
+                            Pixels::Pointer(gav1_image.plane[plane] as *mut u8)
+                        });
                         image.row_bytes[plane] = gav1_image.stride[plane] as u32;
                         image.image_owns_planes[plane] = false;
                     }

src/decoder/mod.rs
@@ -1212,7 +1212,7 @@ impl Decoder {
                             self.image.height = tile.image.height;
                             self.image.depth = tile.image.depth;
                             self.image.yuv_format = tile.image.yuv_format;
-                            self.image.steal_from(&tile.image, category);
+                            self.image.steal_from(&tile.image, category)?;
                             self.image.scale(tile.width, tile.height)?;
                         }
                         Category::Alpha => {
@@ -1220,7 +1220,7 @@ impl Decoder {
                                 println!("Color image item does not match alpha image item");
                                 return Err(AvifError::DecodeAlphaFailed);
                             }
-                            self.image.steal_from(&tile.image, category);
+                            self.image.steal_from(&tile.image, category)?;
                             if !tile.image.full_range {
                                 self.image.alpha_to_full_range()?;
                             }
@@ -1231,7 +1231,7 @@ impl Decoder {
                             self.gainmap.image.height = tile.image.height;
                             self.gainmap.image.depth = tile.image.depth;
                             self.gainmap.image.yuv_format = tile.image.yuv_format;
-                            self.gainmap.image.steal_from(&tile.image, category);
+                            self.gainmap.image.steal_from(&tile.image, category)?;
                             self.gainmap.image.scale(tile.width, tile.height)?;
                         }
                     }

src/image.rs
@@ -77,22 +77,9 @@ pub struct Image {
 
     pub image_sequence_track_present: bool,
     pub progressive_state: ProgressiveState,
-    // TODO: gainmap image ?
 }
 
-pub struct PlaneData<'a> {
-    pub data: Option<&'a [u8]>,
-    pub data16: Option<&'a [u16]>,
-    pub width: u32,
-    pub height: u32,
-    pub row_bytes: u32,
-    pub pixel_size: u32,
-}
-
-// TODO: unify this into the struct above with an enum for mut/const.
-pub struct PlaneMutData<'a> {
-    pub data: Option<&'a mut [u8]>,
-    pub data16: Option<&'a mut [u16]>,
+pub struct PlaneData2 {
     pub width: u32,
     pub height: u32,
     pub row_bytes: u32,
@@ -154,79 +141,55 @@ impl Image {
         }
     }
 
-    pub fn plane(&self, plane: Plane) -> Option<PlaneData> {
+    pub fn plane_data(&self, plane: Plane) -> Option<PlaneData2> {
         if !self.has_plane(plane) {
             return None;
         }
-        let plane_index = plane.to_usize();
-        let pixel_size = if self.depth == 8 { 1 } else { 2 };
-        let height = self.height(plane);
-        let row_bytes = self.row_bytes[plane_index] as usize;
-        let plane_size = height * row_bytes;
-        let planes2 = self.planes2[plane_index].as_ref().unwrap();
-        let (data, data16) = planes2.slices(0, plane_size as u32).unwrap();
-        Some(PlaneData {
-            data,
-            data16,
+        Some(PlaneData2 {
             width: self.width(plane) as u32,
-            height: height as u32,
-            row_bytes: row_bytes as u32,
-            pixel_size,
-        })
-    }
-
-    pub fn plane_mut(&mut self, plane: Plane) -> Option<PlaneMutData> {
-        if !self.has_plane(plane) {
-            return None;
-        }
-        let plane_index = plane.to_usize();
-        let pixel_size = if self.depth == 8 { 1 } else { 2 };
-        let height = self.height(plane);
-        let width = self.width(plane) as u32;
-        let row_bytes = self.row_bytes[plane_index] as usize;
-        let plane_size = height * row_bytes;
-        let planes2 = self.planes2[plane_index].as_mut().unwrap();
-        let (data, data16) = planes2.slices_mut(0, plane_size as u32).unwrap();
-        Some(PlaneMutData {
-            data,
-            data16,
-            width,
-            height: height as u32,
-            row_bytes: row_bytes as u32,
-            pixel_size,
+            height: self.height(plane) as u32,
+            row_bytes: self.row_bytes[plane.to_usize()],
+            pixel_size: if self.depth == 8 { 1 } else { 2 },
         })
     }
 
     pub fn row(&self, plane: Plane, row: u32) -> AvifResult<&[u8]> {
-        let plane = self.plane(plane).ok_or(AvifError::NoContent)?;
-        let row_bytes = usize_from_u32(plane.row_bytes)?;
-        let start = usize_from_u32(row * plane.row_bytes)?;
-        let end = start + row_bytes;
-        Ok(&plane.data.ok_or(AvifError::NoContent)?[start..end])
+        let plane_data = self.plane_data(plane).ok_or(AvifError::NoContent)?;
+        let start = row * plane_data.row_bytes;
+        self.planes2[plane.to_usize()]
+            .as_ref()
+            .unwrap()
+            .slice(start, plane_data.row_bytes)
     }
 
     pub fn row_mut(&mut self, plane: Plane, row: u32) -> AvifResult<&mut [u8]> {
-        let plane = self.plane_mut(plane).ok_or(AvifError::NoContent)?;
-        let row_bytes = usize_from_u32(plane.row_bytes)?;
-        let start = usize_from_u32(row * plane.row_bytes)?;
-        let end = start + row_bytes;
-        Ok(&mut plane.data.ok_or(AvifError::NoContent)?[start..end])
+        let plane_data = self.plane_data(plane).ok_or(AvifError::NoContent)?;
+        let row_bytes = plane_data.row_bytes;
+        let start = row * row_bytes;
+        self.planes2[plane.to_usize()]
+            .as_mut()
+            .unwrap()
+            .slice_mut(start, row_bytes)
     }
 
     pub fn row16(&self, plane: Plane, row: u32) -> AvifResult<&[u16]> {
-        let plane = self.plane(plane).ok_or(AvifError::NoContent)?;
-        let row_bytes = usize_from_u32(plane.row_bytes)? / 2;
-        let start = usize_from_u32(row * plane.row_bytes / 2)?;
-        let end = start + row_bytes;
-        Ok(&plane.data16.ok_or(AvifError::NoContent)?[start..end])
+        let plane_data = self.plane_data(plane).ok_or(AvifError::NoContent)?;
+        let row_bytes = plane_data.row_bytes / 2;
+        let start = row * row_bytes;
+        self.planes2[plane.to_usize()]
+            .as_ref()
+            .unwrap()
+            .slice16(start, row_bytes)
     }
 
     pub fn row16_mut(&mut self, plane: Plane, row: u32) -> AvifResult<&mut [u16]> {
-        let plane = self.plane_mut(plane).ok_or(AvifError::NoContent)?;
-        let row_bytes = usize_from_u32(plane.row_bytes)? / 2;
-        let start = usize_from_u32(row * plane.row_bytes / 2)?;
-        let end = start + row_bytes;
-        Ok(&mut plane.data16.ok_or(AvifError::NoContent)?[start..end])
+        let plane_data = self.plane_data(plane).ok_or(AvifError::NoContent)?;
+        let row_bytes = plane_data.row_bytes / 2;
+        let start = row * row_bytes;
+        self.planes2[plane.to_usize()]
+            .as_mut()
+            .unwrap()
+            .slice16_mut(start, row_bytes)
     }
 
     pub fn allocate_planes(&mut self, category: Category) -> AvifResult<()> {
@@ -260,34 +223,26 @@ impl Image {
         Ok(())
     }
 
-    pub fn steal_from(&mut self, src: &Image, category: Category) {
+    pub fn steal_from(&mut self, src: &Image, category: Category) -> AvifResult<()> {
         // This function is used only when both src and self contains only pointers.
         match category {
             Category::Alpha => {
                 if src.planes2[3].is_some() {
-                    self.planes2[3] =
-                        Some(Pixels::Pointer(src.planes2[3].as_ref().unwrap().pointer()));
+                    self.planes2[3] = src.planes2[3].as_ref().unwrap().clone_pointer();
+                    self.row_bytes[3] = src.row_bytes[3];
                 }
-                self.row_bytes[3] = src.row_bytes[3];
             }
             _ => {
-                if src.planes2[0].is_some() {
-                    self.planes2[0] =
-                        Some(Pixels::Pointer(src.planes2[0].as_ref().unwrap().pointer()));
+                for plane in 0..3usize {
+                    if src.planes2[plane].is_none() {
+                        continue;
+                    }
+                    self.planes2[plane] = src.planes2[plane].as_ref().unwrap().clone_pointer();
+                    self.row_bytes[plane] = src.row_bytes[plane];
                 }
-                if src.planes2[1].is_some() {
-                    self.planes2[1] =
-                        Some(Pixels::Pointer(src.planes2[1].as_ref().unwrap().pointer()));
-                }
-                if src.planes2[2].is_some() {
-                    self.planes2[2] =
-                        Some(Pixels::Pointer(src.planes2[2].as_ref().unwrap().pointer()));
-                }
-                self.row_bytes[0] = src.row_bytes[0];
-                self.row_bytes[1] = src.row_bytes[1];
-                self.row_bytes[2] = src.row_bytes[2];
             }
         }
+        Ok(())
     }
 
     pub fn copy_from_tile(
@@ -304,7 +259,7 @@ impl Image {
         //println!("copying tile {tile_index} {row_index} {column_index}");
         for plane in category.planes() {
             let plane = *plane;
-            let src_plane = tile.plane(plane);
+            let src_plane = tile.plane_data(plane);
             if src_plane.is_none() {
                 continue;
             }

src/internal_utils/mod.rs
@@ -112,6 +112,7 @@ conversion_function!(u32_from_i32, u32, i32);
 conversion_function!(i32_from_u32, i32, u32);
 #[cfg(feature = "android_mediacodec")]
 conversion_function!(isize_from_i32, isize, i32);
+#[cfg(feature = "capi")]
 conversion_function!(isize_from_u32, isize, u32);
 conversion_function!(isize_from_usize, isize, usize);
 

src/reformat/alpha.rs
@@ -5,7 +5,6 @@ use super::rgb;
 
 use crate::decoder::Category;
 use crate::image::Plane;
-use crate::internal_utils::pixels::*;
 use crate::internal_utils::*;
 use crate::*;
 
@@ -170,7 +169,7 @@ impl image::Image {
                     None,
                     None,
                     None,
-                    Some(Pixels::Pointer(self.planes2[3].as_ref().unwrap().pointer())),
+                    self.planes2[3].as_ref().unwrap().clone_pointer(),
                 ],
                 row_bytes: [0, 0, 0, self.row_bytes[3]],
                 ..image::Image::default()
@@ -216,6 +215,8 @@ impl image::Image {
 mod tests {
     use super::*;
 
+    use crate::internal_utils::pixels::*;
+
     use rand::Rng;
     use test_case::test_matrix;
 
@@ -247,7 +248,11 @@ mod tests {
             buffer.reserve_exact(buffer_size);
             buffer.resize(buffer_size, 0);
             // Use a pointer to mimic C API calls.
-            rgb.pixels = Some(Pixels::Pointer(buffer.as_mut_ptr()));
+            rgb.pixels = Some(if rgb.depth > 8 {
+                Pixels::Pointer16(buffer.as_mut_ptr() as *mut u16)
+            } else {
+                Pixels::Pointer(buffer.as_mut_ptr())
+            });
             rgb.row_bytes = width * 4 * pixel_size;
         } else {
             rgb.allocate()?;

commit af2241b368d242e3776246bfb1976a43d4bc8685

capi: Use the correct Pixel type based on bit depth
This was broken since https://github.com/webmproject/CrabbyAvif/pull/50.

PiperOrigin-RevId: 616916586

src/capi/reformat.rs
@@ -64,7 +64,7 @@ impl From<*mut avifRGBImage> for rgb::Image {
             premultiply_alpha: rgb.alpha_premultiplied,
             is_float: rgb.is_float,
             max_threads: rgb.max_threads,
-            pixels: Some(Pixels::Pointer(rgb.pixels)),
+            pixels: Some(Pixels::from_raw_pointer(rgb.pixels, rgb.depth)),
             row_bytes: rgb.row_bytes,
         };
         let format = match (rgb.format, rgb.ignore_alpha) {
@@ -97,10 +97,10 @@ impl From<*const avifImage> for image::Image {
             alpha_present: !image.alphaPlane.is_null(),
             alpha_premultiplied: image.alphaPremultiplied == AVIF_TRUE,
             planes: [
-                Some(Pixels::Pointer(image.yuvPlanes[0])),
-                Some(Pixels::Pointer(image.yuvPlanes[1])),
-                Some(Pixels::Pointer(image.yuvPlanes[2])),
-                Some(Pixels::Pointer(image.alphaPlane)),
+                Some(Pixels::from_raw_pointer(image.yuvPlanes[0], image.depth)),
+                Some(Pixels::from_raw_pointer(image.yuvPlanes[1], image.depth)),
+                Some(Pixels::from_raw_pointer(image.yuvPlanes[2], image.depth)),
+                Some(Pixels::from_raw_pointer(image.alphaPlane, image.depth)),
             ],
             row_bytes: [
                 image.yuvRowBytes[0],

src/internal_utils/pixels.rs
@@ -13,6 +13,14 @@ pub enum Pixels {
 }
 
 impl Pixels {
+    pub fn from_raw_pointer(ptr: *mut u8, depth: u32) -> Self {
+        if depth > 8 {
+            Pixels::Pointer16(ptr as *mut u16)
+        } else {
+            Pixels::Pointer(ptr)
+        }
+    }
+
     pub fn size(&self) -> usize {
         match self {
             Pixels::Pointer(_) => 0,