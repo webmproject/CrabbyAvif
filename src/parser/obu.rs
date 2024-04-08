33abf6da677239f4769ca85be7f73debd66ef0ba
Yannis Guyon <yguyon@google.com>
2024-04-08 18:55:52 +0200

    Lint obu.rs
    Use ignored vars instead of comments for consistency.

--- src/parser/obu.rs
+++ src/parser/obu.rs
@@ -34,39 +34,37 @@ impl Av1SequenceHeader {
         let still_picture = bits.read_bool()?;
         self.reduced_still_picture_header = bits.read_bool()?;
         if self.reduced_still_picture_header && !still_picture {
-            return Err(AvifError::BmffParseFailed("".into()));
+            return Err(AvifError::BmffParseFailed(
+                "invalid reduced_still_picture_header".into(),
+            ));
         }
         if self.reduced_still_picture_header {
             self.config.seq_level_idx0 = bits.read(5)? as u8;
         } else {
             let mut buffer_delay_length = 0;
-            let mut decoder_model_info_present = false;
-            // timing_info_present_flag
-            if bits.read_bool()? {
-                // num_units_in_display_tick
-                bits.skip(32)?;
-                // time_scale
-                bits.skip(32)?;
-                // equal_picture_interval
-                if bits.read_bool()? {
-                    // num_ticks_per_picture
-                    bits.skip_uvlc()?;
+            let mut decoder_model_info_present_flag = false;
+            let timing_info_present_flag = bits.read_bool()?;
+            if timing_info_present_flag {
+                let _num_units_in_display_tick = bits.skip(32)?;
+                let _time_scale = bits.skip(32)?;
+                let equal_picture_interval = bits.read_bool()?;
+                if equal_picture_interval {
+                    let _num_ticks_per_picture_minus_1 = bits.skip_uvlc()?;
                 }
-                // decoder_model_info_present_flag
-                decoder_model_info_present = bits.read_bool()?;
-                if decoder_model_info_present {
-                    buffer_delay_length = bits.read(5)? + 1;
-                    // num_units_in_decoding_tick
-                    bits.skip(32)?;
-                    // buffer_removal_time_length_minus_1, frame_presentation_time_length_minus_1
-                    bits.skip(10)?;
+                decoder_model_info_present_flag = bits.read_bool()?;
+                if decoder_model_info_present_flag {
+                    let buffer_delay_length_minus_1 = bits.read(5)?;
+                    buffer_delay_length = buffer_delay_length_minus_1 + 1;
+                    let _num_units_in_decoding_tick = bits.skip(32)?;
+                    let _buffer_removal_time_length_minus_1 = bits.skip(5)?;
+                    let _frame_presentation_time_length_minus_1 = bits.skip(5)?;
                 }
             }
-            let initial_display_delay_present = bits.read_bool()?;
-            let operaing_points_count = bits.read(5)? + 1;
-            for i in 0..operaing_points_count {
-                // operating_point_idc
-                bits.skip(12)?;
+            let initial_display_delay_present_flag = bits.read_bool()?;
+            let operating_points_cnt_minus_1 = bits.read(5)?;
+            let operating_points_cnt = operating_points_cnt_minus_1 + 1;
+            for i in 0..operating_points_cnt {
+                let _operating_point_idc = bits.skip(12)?;
                 let seq_level_idx = bits.read(5)?;
                 if i == 0 {
                     self.config.seq_level_idx0 = seq_level_idx as u8;
@@ -77,22 +75,18 @@ impl Av1SequenceHeader {
                         self.config.seq_tier0 = seq_tier as u8;
                     }
                 }
-                if decoder_model_info_present {
-                    // decoder_model_present_for_this_op
-                    if bits.read_bool()? {
-                        // decoder_buffer_delay
-                        bits.skip(buffer_delay_length as usize)?;
-                        // encoder_buffer_delay
-                        bits.skip(buffer_delay_length as usize)?;
-                        // low_delay_mode_flag
-                        bits.skip(1)?;
+                if decoder_model_info_present_flag {
+                    let decoder_model_present_for_this_op = bits.read_bool()?;
+                    if decoder_model_present_for_this_op {
+                        let _decoder_buffer_delay = bits.skip(buffer_delay_length as usize)?;
+                        let _encoder_buffer_delay = bits.skip(buffer_delay_length as usize)?;
+                        let _low_delay_mode_flag = bits.skip(1)?;
                     }
                 }
-                if initial_display_delay_present {
-                    // initial_display_delay_present_for_this_op
-                    if bits.read_bool()? {
-                        // initial_display_delay_minus_1
-                        bits.skip(4)?;
+                if initial_display_delay_present_flag {
+                    let initial_display_delay_present_for_this_op = bits.read_bool()?;
+                    if initial_display_delay_present_for_this_op {
+                        let _initial_display_delay_minus_1 = bits.skip(4)?;
                     }
                 }
             }
@@ -101,46 +95,55 @@ impl Av1SequenceHeader {
     }
 
     fn parse_frame_max_dimensions(&mut self, bits: &mut IBitStream) -> AvifResult<()> {
-        let frame_width_bits = bits.read(4)? + 1;
-        let frame_height_bits = bits.read(4)? + 1;
-        self.max_width = bits.read(frame_width_bits as usize)? + 1;
-        self.max_height = bits.read(frame_height_bits as usize)? + 1;
-        let mut frame_id_numbers_present = false;
-        if !self.reduced_still_picture_header {
-            frame_id_numbers_present = bits.read_bool()?;
+        let frame_width_bits_minus_1 = bits.read(4)?;
+        let frame_height_bits_minus_1 = bits.read(4)?;
+        let max_frame_width_minus_1 = bits.read(frame_width_bits_minus_1 as usize + 1)?;
+        let max_frame_height_minus_1 = bits.read(frame_height_bits_minus_1 as usize + 1)?;
+        self.max_width = max_frame_width_minus_1 + 1;
+        self.max_height = max_frame_height_minus_1 + 1;
+        let frame_id_numbers_present_flag;
+        if self.reduced_still_picture_header {
+            frame_id_numbers_present_flag = false;
+        } else {
+            frame_id_numbers_present_flag = bits.read_bool()?;
         }
-        if frame_id_numbers_present {
-            // delta_frame_id_length_minus_2, additional_frame_id_length_minus_1
-            bits.skip(7)?;
+        if frame_id_numbers_present_flag {
+            let _delta_frame_id_length_minus_2 = bits.skip(4)?;
+            let _additional_frame_id_length_minus_1 = bits.skip(3)?;
         }
         Ok(())
     }
 
     fn parse_enabled_features(&mut self, bits: &mut IBitStream) -> AvifResult<()> {
-        // use_128x128_superblock, enable_filter_intra, enable_intra_edge_filter
-        bits.skip(3)?;
+        let _use_128x128_superblock = bits.skip(1)?;
+        let _enable_filter_intra = bits.skip(1)?;
+        let _enable_intra_edge_filter = bits.skip(1)?;
         if self.reduced_still_picture_header {
             return Ok(());
         }
-        // enable_interintra_compound, enable_masked_compound
-        // enable_warped_motion, enable_dual_filter
-        bits.skip(4)?;
+        let _enable_interintra_compound = bits.skip(1)?;
+        let _enable_masked_compound = bits.skip(1)?;
+        let _enable_warped_motion = bits.skip(1)?;
+        let _enable_dual_filter = bits.skip(1)?;
         let enable_order_hint = bits.read_bool()?;
         if enable_order_hint {
-            // enable_jnt_comp, enable_ref_frame_mvs
-            bits.skip(2)?;
+            let _enable_jnt_comp = bits.skip(1)?;
+            let _enable_ref_frame_mvs = bits.skip(1)?;
         }
-        let seq_force_screen_content_tools = if bits.read_bool()? { 2 } else { bits.read(1)? };
+        let seq_choose_screen_content_tools = bits.read_bool()?;
+        let seq_force_screen_content_tools = if seq_choose_screen_content_tools {
+            2 // SELECT_SCREEN_CONTENT_TOOLS
+        } else {
+            bits.read(1)?
+        };
         if seq_force_screen_content_tools > 0 {
-            // seq_choose_integer_mv
-            if !bits.read_bool()? {
-                // seq_force_integer_mv
-                bits.skip(1)?;
+            let seq_choose_integer_mv = bits.read_bool()?;
+            if !seq_choose_integer_mv {
+                let _seq_force_integer_mv = bits.skip(1)?;
             }
         }
         if enable_order_hint {
-            // order_hint_bits_minus_1
-            bits.skip(3)?;
+            let _order_hint_bits_minus_1 = bits.skip(3)?;
         }
         Ok(())
     }
@@ -156,13 +159,10 @@ impl Av1SequenceHeader {
         if self.config.seq_profile != 1 {
             self.config.monochrome = bits.read_bool()?;
         }
-        // color_description_present_flag
-        if bits.read_bool()? {
-            // color_primaries
+        let color_description_present_flag = bits.read_bool()?;
+        if color_description_present_flag {
             self.color_primaries = (bits.read(8)? as u16).into();
-            // transfer_characteristics
             self.transfer_characteristics = (bits.read(8)? as u16).into();
-            // matrix_coefficients
             self.matrix_coefficients = (bits.read(8)? as u16).into();
         } else {
             self.color_primaries = ColorPrimaries::Unspecified;
@@ -170,20 +170,20 @@ impl Av1SequenceHeader {
             self.matrix_coefficients = MatrixCoefficients::Unspecified;
         }
         if self.config.monochrome {
-            self.full_range = bits.read_bool()?;
+            let color_range = bits.read_bool()?;
+            self.full_range = color_range;
             self.config.chroma_subsampling_x = 1;
             self.config.chroma_subsampling_y = 1;
             self.yuv_format = PixelFormat::Monochrome;
-            return Ok(());
-        }
-        if self.color_primaries == ColorPrimaries::Srgb
+        } else if self.color_primaries == ColorPrimaries::Bt709
             && self.transfer_characteristics == TransferCharacteristics::Srgb
             && self.matrix_coefficients == MatrixCoefficients::Identity
         {
             self.full_range = true;
             self.yuv_format = PixelFormat::Yuv444;
         } else {
-            self.full_range = bits.read_bool()?;
+            let color_range = bits.read_bool()?;
+            self.full_range = color_range;
             match self.config.seq_profile {
                 0 => {
                     self.config.chroma_subsampling_x = 1;
@@ -218,33 +218,37 @@ impl Av1SequenceHeader {
                 self.config.chroma_sample_position = bits.read(2)?.into();
             }
         }
-        // separate_uv_delta_q
-        bits.skip(1)?;
+        let _separate_uv_delta_q = bits.skip(1)?;
         Ok(())
     }
 
     fn parse_obu_header(stream: &mut IStream) -> AvifResult<ObuHeader> {
         let mut bits = stream.sub_bit_stream(1)?;
-        // obu_forbidden_bit
-        bits.skip(1)?;
-        // obu_type
+
+        // Section 5.3.2 of AV1 specification.
+        // https://aomediacodec.github.io/av1-spec/#obu-header-syntax
+        let obu_forbidden_bit = bits.read(1)?;
+        if obu_forbidden_bit != 0 {
+            return Err(AvifError::BmffParseFailed(
+                "invalid obu_forbidden_bit".into(),
+            ));
+        }
         let obu_type = bits.read(4)? as u8;
-        // obu_extension_flag
         let obu_extension_flag = bits.read_bool()?;
-        // obu_has_size_field
         let obu_has_size_field = bits.read_bool()?;
-        // obu_reserved_1bit
-        bits.skip(1)?;
+        let _obu_reserved_1bit = bits.skip(1)?; // "The value is ignored by a decoder."
 
         if obu_extension_flag {
-            // temporal_id, spatial_id, extension_header_reserved_3bits
-            stream.skip(1)?;
+            let mut bits = stream.sub_bit_stream(1)?;
+            let _temporal_id = bits.skip(3)?;
+            let _spatial_id = bits.skip(2)?;
+            let _extension_header_reserved_3bits = bits.skip(3)?;
         }
 
         let size = if obu_has_size_field {
             stream.read_uleb128()?
         } else {
-            u32_from_usize(stream.bytes_left()?)?
+            u32_from_usize(stream.bytes_left()?)? // sz - 1 - obu_extension_flag
         };
 
         Ok(ObuHeader { obu_type, size })
@@ -255,7 +259,7 @@ impl Av1SequenceHeader {
 
         while stream.has_bytes_left()? {
             let obu = Self::parse_obu_header(&mut stream)?;
-            if obu.obu_type != 1 {
+            if obu.obu_type != /*OBU_SEQUENCE_HEADER=*/1 {
                 // Not a sequence header. Skip this obu.
                 stream.skip(usize_from_u32(obu.size)?)?;
                 continue;
@@ -265,14 +269,13 @@ impl Av1SequenceHeader {
             sequence_header.parse_profile(&mut bits)?;
             sequence_header.parse_frame_max_dimensions(&mut bits)?;
             sequence_header.parse_enabled_features(&mut bits)?;
-            // enable_superres, enable_cdef, enable_restoration
-            bits.skip(3)?;
+            let _enable_superres = bits.skip(1)?;
+            let _enable_cdef = bits.skip(1)?;
+            let _enable_restoration = bits.skip(1)?;
             sequence_header.parse_color_config(&mut bits)?;
-            // film_grain_params_present
-            bits.skip(1)?;
+            let _film_grain_params_present = bits.skip(1)?;
             return Ok(sequence_header);
         }
-        // Failed to parse a sequence header.
         Err(AvifError::BmffParseFailed(
             "could not parse sequence header".into(),
         ))
