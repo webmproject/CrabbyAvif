fd6b719f25909a316ff27bbf70f137bcf96f5c1e
Vignesh Venkat <vigneshv@google.com>
2025-02-04 11:40:13 -0800

    Add decoder utility
    Add a decoder utility that has roughly the same options as
    avifdec. Only the `--info` mode is implemented as of now.
    
    PiperOrigin-RevId: 723169424

---
+++ examples/crabby_decode.rs
@@ -0,0 +1,291 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use clap::Parser;
+
+use crabby_avif::decoder::track::RepetitionCount;
+use crabby_avif::decoder::*;
+use crabby_avif::*;
+
+#[derive(Parser)]
+struct CommandLineArgs {
+    /// Disable strict decoding, which disables strict validation checks and errors
+    #[arg(long)]
+    no_strict: Option<bool>,
+
+    /// Decode all frames and display all image information instead of saving to disk
+    #[arg(short, long, default_value = "false")]
+    info: bool,
+
+    /// Input AVIF file
+    #[arg(allow_hyphen_values = false)]
+    input_file: String,
+
+    /// Output file
+    #[arg(allow_hyphen_values = false)]
+    output_file: Option<String>,
+
+    #[arg(long)]
+    jobs: Option<u32>,
+}
+
+fn print_data_as_columns(rows: &[(usize, &str, String)]) {
+    let rows: Vec<_> = rows
+        .iter()
+        .filter(|x| !x.1.is_empty())
+        .map(|x| (format!("{} * {}", " ".repeat(x.0 * 4), x.1), x.2.as_str()))
+        .collect();
+
+    // Calculate the maximum width for the first column.
+    let mut max_col1_width = 0;
+    for (col1, _) in &rows {
+        max_col1_width = max_col1_width.max(col1.len());
+    }
+
+    for (col1, col2) in &rows {
+        println!("{col1:<max_col1_width$} : {col2}");
+    }
+}
+
+fn print_vec(data: &[u8]) -> String {
+    if data.is_empty() {
+        format!("Absent")
+    } else {
+        format!("Present ({} bytes)", data.len())
+    }
+}
+
+fn print_image_info(decoder: &Decoder) {
+    let image = decoder.image().unwrap();
+    let mut image_data = vec![
+        (0, "Resolution", format!("{}x{}", image.width, image.height)),
+        (0, "Bit Depth", format!("{}", image.depth)),
+        (0, "Format", format!("{:#?}", image.yuv_format)),
+        if image.yuv_format == PixelFormat::Yuv420 {
+            (
+                0,
+                "Chroma Sample Position",
+                format!("{:#?}", image.chroma_sample_position),
+            )
+        } else {
+            (0, "", "".into())
+        },
+        (
+            0,
+            "Alpha",
+            format!(
+                "{}",
+                match (image.alpha_present, image.alpha_premultiplied) {
+                    (true, true) => "Premultiplied",
+                    (true, false) => "Not premultiplied",
+                    (false, _) => "Absent",
+                }
+            ),
+        ),
+        (0, "Range", format!("{:#?}", image.yuv_range)),
+        (
+            0,
+            "Color Primaries",
+            format!("{:#?}", image.color_primaries),
+        ),
+        (
+            0,
+            "Transfer Characteristics",
+            format!("{:#?}", image.transfer_characteristics),
+        ),
+        (
+            0,
+            "Matrix Coefficients",
+            format!("{:#?}", image.matrix_coefficients),
+        ),
+        (0, "ICC Profile", print_vec(&image.icc)),
+        (0, "XMP Metadata", print_vec(&image.xmp)),
+        (0, "Exif Metadata", print_vec(&image.exif)),
+    ];
+    if image.pasp.is_none()
+        && image.clap.is_none()
+        && image.irot_angle.is_none()
+        && image.imir_axis.is_none()
+    {
+        image_data.push((0, "Transformations", format!("None")));
+    } else {
+        image_data.push((0, "Transformations", format!("")));
+        if let Some(pasp) = image.pasp {
+            image_data.push((
+                1,
+                "pasp (Aspect Ratio)",
+                format!("{}/{}", pasp.h_spacing, pasp.v_spacing),
+            ));
+        }
+        if let Some(_clap) = image.clap {
+            // TODO: b/394162563 - print clap info.
+            image_data.push((1, "clap (Clean Aperture)", format!("")));
+        }
+        if let Some(angle) = image.irot_angle {
+            image_data.push((1, "irot (Rotation)", format!("{angle}")));
+        }
+        if let Some(axis) = image.imir_axis {
+            image_data.push((1, "imir (Mirror)", format!("{axis}")));
+        }
+    }
+    image_data.push((0, "Progressive", format!("{:#?}", image.progressive_state)));
+    if let Some(clli) = image.clli {
+        image_data.push((0, "CLLI", format!("{}, {}", clli.max_cll, clli.max_pall)));
+    }
+    if decoder.gainmap_present() {
+        let gainmap = decoder.gainmap();
+        let gainmap_image = &gainmap.image;
+        image_data.extend_from_slice(&[
+            (
+                0,
+                "Gainmap",
+                format!(
+                "{}x{} pixels, {} bit, {:#?}, {:#?} Range, Matrix Coeffs. {:#?}, Base Image is {}",
+                gainmap_image.width,
+                gainmap_image.height,
+                gainmap_image.depth,
+                gainmap_image.yuv_format,
+                gainmap_image.yuv_range,
+                gainmap_image.matrix_coefficients,
+                if gainmap.metadata.base_hdr_headroom.0 == 0 { "SDR" } else { "HDR" },
+            ),
+            ),
+            (0, "Alternate image", format!("")),
+            (
+                1,
+                "Color Primaries",
+                format!("{:#?}", gainmap.alt_color_primaries),
+            ),
+            (
+                1,
+                "Transfer Characteristics",
+                format!("{:#?}", gainmap.alt_transfer_characteristics),
+            ),
+            (
+                1,
+                "Matrix Coefficients",
+                format!("{:#?}", gainmap.alt_matrix_coefficients),
+            ),
+            (1, "ICC Profile", print_vec(&gainmap.alt_icc)),
+            (1, "Bit Depth", format!("{}", gainmap.alt_plane_depth)),
+            (1, "Planes", format!("{}", gainmap.alt_plane_count)),
+            if let Some(clli) = gainmap_image.clli {
+                (1, "CLLI", format!("{}, {}", clli.max_cll, clli.max_pall))
+            } else {
+                (1, "", "".into())
+            },
+        ])
+    } else {
+        // TODO: b/394162563 - check if we need to report the present but ignored case.
+        image_data.push((0, "Gainmap", format!("Absent")));
+    }
+    if image.image_sequence_track_present {
+        image_data.push((
+            0,
+            "Repeat Count",
+            match decoder.repetition_count() {
+                RepetitionCount::Finite(x) => format!("{x}"),
+                RepetitionCount::Infinite => format!("Infinite"),
+                RepetitionCount::Unknown => format!("Unknown"),
+            },
+        ));
+    }
+    print_data_as_columns(&image_data);
+}
+
+fn info(args: &CommandLineArgs) -> bool {
+    if args.output_file.is_some() {
+        eprintln!("output_file is not allowed with --info");
+        return false;
+    }
+
+    let settings = Settings {
+        strictness: if args.no_strict.unwrap_or(false) {
+            Strictness::None
+        } else {
+            Strictness::All
+        },
+        image_content_to_decode: ImageContentType::All,
+        max_threads: args.jobs.unwrap_or(1),
+        ..Settings::default()
+    };
+    let mut decoder = Decoder::default();
+    decoder.settings = settings;
+    if let Err(err) = decoder.set_io_file(&args.input_file) {
+        eprintln!("ERROR: Cannot open input file: {:#?}", err);
+        return false;
+    }
+    if let Err(err) = decoder.parse() {
+        eprintln!("ERROR: Failed to parse image: {:#?}", err);
+        return false;
+    }
+    println!("Image decoded: {}", args.input_file);
+    print_image_info(&decoder);
+    println!(
+        " * {} timescales per second, {} seconds ({} timescales), {} frame{}",
+        decoder.timescale(),
+        decoder.duration(),
+        decoder.duration_in_timescales(),
+        decoder.image_count(),
+        if decoder.image_count() == 1 { "" } else { "s" },
+    );
+    if decoder.image_count() > 1 {
+        let image = decoder.image().unwrap();
+        println!(
+            " * {} Frames: ({} expected frames)",
+            if image.image_sequence_track_present {
+                "Image Sequence"
+            } else {
+                "Progressive Image"
+            },
+            decoder.image_count()
+        );
+    } else {
+        println!(" * Frame:");
+    }
+
+    let mut index = 0;
+    loop {
+        match decoder.next_image() {
+            Ok(_) => {
+                println!("     * Decoded frame [{}] [pts {} ({} timescales)] [duration {} ({} timescales)] [{}x{}]",
+                    index,
+                    decoder.image_timing().pts,
+                    decoder.image_timing().pts_in_timescales,
+                    decoder.image_timing().duration,
+                    decoder.image_timing().duration_in_timescales,
+                    decoder.image().unwrap().width,
+                    decoder.image().unwrap().height);
+                index += 1;
+            }
+            Err(AvifError::NoImagesRemaining) => {
+                return true;
+            }
+            Err(err) => {
+                eprintln!("ERROR: Failed to decode frame: {:#?}", err);
+                return false;
+            }
+        }
+    }
+}
+
+fn decode(_args: &CommandLineArgs) -> bool {
+    todo!("Implement decoding to file");
+}
+
+fn main() {
+    let args = CommandLineArgs::parse();
+    let success = if args.info { info(&args) } else { decode(&args) };
+    std::process::exit(if success { 0 } else { 1 });
+}
