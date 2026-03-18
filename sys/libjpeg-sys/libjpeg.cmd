7034c189da89d976306dca6a18540aca45708445
Vignesh Venkat <vigneshv@google.com>
2026-03-18 13:29:35 -0700

    Revert: "Add libjpeg-sys for JPG support"
    PiperOrigin-RevId: 885759929

--- sys/libjpeg-sys/libjpeg.cmd
+++
@@ -1,4 +0,0 @@
-git clone -b 3.1.3 --depth 1 https://github.com/libjpeg-turbo/libjpeg-turbo.git
-
-cmake -G Ninja -S libjpeg-turbo -B libjpeg-turbo/build -DENABLE_SHARED=OFF -DENABLE_STATIC=ON -DCMAKE_BUILD_TYPE=Release -DWITH_TURBOJPEG=OFF -DWITH_CRT_DLL=ON
-cmake --build libjpeg-turbo/build --config Release --parallel
