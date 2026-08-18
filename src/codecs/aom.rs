45b8b958cc1242dcf6243244070a8702278eb9bd
Wan-Teh Chang <wtc@google.com>
2026-08-18 13:17:50 -0700

    Don't need to emulate libaom all-intra usage mode
    Unlike libavif, CrabbyAvif only supports versions of libaom that have
    AOM_USAGE_ALL_INTRA. Delete the code that emulates three settings of
    AOM_USAGE_ALL_INTRA.
    
    Change the condition for setting keyframe interval to
    aom_usage != AOM_USAGE_ALL_INTRA because it is semantically more
    correct and will be future-proof if we ever use AOM_USAGE_ALL_INTRA to
    encode multiple frames.
    
    PiperOrigin-RevId: 966767959

--- src/codecs/aom.rs
+++ src/codecs/aom.rs
@@ -186,13 +186,9 @@ impl Encoder for Aom {
 
             if config.is_single_image {
                 aom_config.g_limit = 1;
-                aom_config.g_lag_in_frames = 0;
-                aom_config.kf_mode = aom_kf_mode_AOM_KF_DISABLED;
-                aom_config.kf_max_dist = 0;
-            } else {
-                if config.keyframe_interval > 0 {
-                    aom_config.kf_max_dist = config.keyframe_interval as u32;
-                }
+            }
+            if aom_usage != AOM_USAGE_ALL_INTRA && config.keyframe_interval > 0 {
+                aom_config.kf_max_dist = config.keyframe_interval as u32;
             }
             if config.disable_lagged_output {
                 aom_config.g_lag_in_frames = 0;
